7a3bd4d85287be5a89b2c109c5dd40630578d8df
sunan <sunanhaqi.sh@gmail.com>
2022-08-15 17:40:58 +0700

    Santri 2.8

--- app/src/main/java/com/example/santri/ui/view/DetailActivity.kt
+++ app/src/main/java/com/example/santri/ui/view/DetailActivity.kt
@@ -3,7 +3,12 @@ package com.example.santri.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.activity.viewModels
+import com.example.santri.R
 import com.example.santri.databinding.ActivityDetailBinding
 import com.example.santri.network.model.SantriItem
 import com.example.santri.ui.viewmodel.DetailViewModel
@@ -15,6 +20,34 @@ class DetailActivity : AppCompatActivity() {
     private val viewModel: DetailViewModel by viewModels()
     private lateinit var binding: ActivityDetailBinding
 
+    //animation
+    private val rotateOpen: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            this,
+            R.anim.rotate_open_anim
+        )
+    }
+    private val rotateClose: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            this,
+            R.anim.rotate_close_anim
+        )
+    }
+    private val fromBottom: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            this,
+            R.anim.from_bottom_anim
+        )
+    }
+    private val toBottom: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            this,
+            R.anim.to_bottom_anim
+        )
+    }
+
+    private var clicked = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
@@ -23,6 +56,20 @@ class DetailActivity : AppCompatActivity() {
         supportActionBar?.title = TITLE
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        binding.fabMenu.setOnClickListener {
+            onFabMenuClicked()
+        }
+
+        //Animation FAB
+        binding.fabEdit.setOnClickListener {
+            Toast.makeText(this, "Edit Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.fabDelete.setOnClickListener {
+            Toast.makeText(this, "Delete Button Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        //Get Detail Data
         val data = intent.getParcelableExtra(EXTRA_DATA) as? SantriItem
         viewModel.getDetailUser().observe(this) { response ->
             if (response != null) {
@@ -36,6 +83,47 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
+    //Animation FAB
+    private fun onFabMenuClicked() {
+        setVisibility(clicked)
+        setAnimation(clicked)
+        setClickable(clicked)
+        clicked = !clicked
+    }
+
+    private fun setVisibility(clicked: Boolean) {
+        if (clicked) {
+            binding.fabEdit.visibility = View.VISIBLE
+            binding.fabDelete.visibility = View.VISIBLE
+        } else {
+            binding.fabEdit.visibility = View.INVISIBLE
+            binding.fabDelete.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun setAnimation(clicked: Boolean) {
+        if (!clicked) {
+            binding.fabEdit.startAnimation(fromBottom)
+            binding.fabDelete.startAnimation(fromBottom)
+            binding.fabMenu.startAnimation(rotateOpen)
+        } else {
+            binding.fabEdit.startAnimation(toBottom)
+            binding.fabDelete.startAnimation(toBottom)
+            binding.fabMenu.startAnimation(rotateClose)
+        }
+    }
+
+    private fun setClickable(clicked: Boolean) {
+        if (!clicked) {
+            binding.fabEdit.isClickable = true
+            binding.fabDelete.isClickable = true
+        } else {
+            binding.fabEdit.isClickable = false
+            binding.fabDelete.isClickable = false
+        }
+    }
+
+    //Post Data to View
     private fun view(data: SantriItem) {
         with(binding) {
             nis.text = data.nis
